9acd1a62fdd5dece58b21a2a5fefc16575afbccc
BatttA <51757016+BatttA@users.noreply.github.com>
2019-07-03 22:54:20 +0400

    Second
    Add new variable

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
+        int z = 15;
+        System.out.println(z);
         System.out.println("Add first branch!");
         System.out.println("Hello World!");
     }
